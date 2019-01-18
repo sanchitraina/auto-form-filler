4de29e01e6d3962df0beb8e55d4ecd5b61f1f099
sanchit_raina <rainasanchit01@gmail.com>
2019-01-18 11:34:59 +0530

    Updated requred repos

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ except ImportError:
 config = {
 	'description': 'Automatically Fill All Form Fields',
 	'author': 'Sanchit',
-	'url': 'URL to get it at.',
+	'url': 'bin\\index.py',
 	'download_url': 'Where to download it.',
 	'author_email': 'rainasanchit01@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
-	'packages': ['NAME'],
+	'packages': ['virtualenv'],
 	'scripts': [],
 	'name': 'autoformfiller'
 }
